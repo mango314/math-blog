4977033cc136024bfdb238f34f98c1403364a882
JM <john.mangual@gmail.com>
2018-10-20 11:01:36 -0400

    Schur polynomials (tutorial)

--- 2018/symmetric.py
+++ 2018/symmetric.py
@@ -24,3 +24,47 @@ Type "help", "copyright", "credits" or "license" for more information.
 >>> X = (a+b+c)**3 +2*(a**3+b**3+c**3) - 3*(a+b+c)*(a**2+b**2+c**2)
 >>> expand(X)
 6*a*b*c
+>>> from sympy import *
+>>> a = symbol('a')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: 'module' object is not callable
+>>> a = symbols('a')
+>>> b = symbols('b')
+>>> c = symbols('c')
+>>> def det((a,b,c),(d,e,f),(g,h,i)):
+...     return a*e*i + b*f*g + c*d*h - c*e*g - c*f*h - b*d*i
+... 
+>>> A = ((a**4,b**4,c**4),(a**2,b**2,c**2),(1,1,1))
+>>> B = ((a**2,b**2,c**2),(a,b,c),(1,1,1))
+>>> det(A)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: det() takes exactly 3 arguments (1 given)
+>>> def det(((a,b,c),(d,e,f),(g,h,i))):
+...     return a*e*i + b*f*g + c*d*h - c*e*g - c*f*h - b*d*i
+... det(A)
+  File "<stdin>", line 3
+    det(A)
+      ^
+SyntaxError: invalid syntax
+>>> def det(((a,b,c),(d,e,f),(g,h,i))):
+...     return a*e*i + b*f*g + c*d*h - c*e*g - c*f*h - b*d*i
+... 
+>>> det(A)
+a**4*b**2 - a**2*b**4 + a**2*c**4 + b**4*c**2 - b**2*c**4 - c**6
+>>> det(B)
+a**2*b - a*b**2 + a*c**2 + b**2*c - b*c**2 - c**3
+>>> expand(det(A)/det(B))
+a**4*b**2/(a**2*b - a*b**2 + a*c**2 + b**2*c - b*c**2 - c**3) - a**2*b**4/(a**2*b - a*b**2 + a*c**2 + b**2*c - b*c**2 - c**3) + a**2*c**4/(a**2*b - a*b**2 + a*c**2 + b**2*c - b*c**2 - c**3) + b**4*c**2/(a**2*b - a*b**2 + a*c**2 + b**2*c - b*c**2 - c**3) - b**2*c**4/(a**2*b - a*b**2 + a*c**2 + b**2*c - b*c**2 - c**3) - c**6/(a**2*b - a*b**2 + a*c**2 + b**2*c - b*c**2 - c**3)
+>>> simplify(det(A)/det(B))
+(a**3*b**2 + a**2*b**2*c - a*b**4 + a*b**2*c**2 + a*c**4 - b**4*c + b**2*c**3 + c**5)/(a*b - b**2 + b*c + c**2)
+>>> B
+((a**2, b**2, c**2), (a, b, c), (1, 1, 1))
+>>> factor(det(B))
+(a - c)*(a*b - b**2 + b*c + c**2)
+>>> def det(((a,b,c),(d,e,f),(g,h,i))):
+...     return a*e*i + b*f*g + c*d*h - c*e*g - a*f*h - b*d*i
+... 
+>>> simplify(det(A)/det(B))
+a**2*b + a**2*c + a*b**2 + 2*a*b*c + a*c**2 + b**2*c + b*c**2
